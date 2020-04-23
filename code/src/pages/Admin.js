f5b1598ddda2817066806d7b07ff1d3b38f199a2
Naveen Janarthanan <navjanar@uw.edu>
2020-04-22 23:34:41 -0700

    Removed packages from the admin page that we dont need

--- code/src/pages/Admin.js
+++ code/src/pages/Admin.js
@@ -4,20 +4,7 @@ import NavBar from "../components/Navbar";
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
-
-
-
-
-// Test
 import Paper from '@material-ui/core/Paper';
-
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
