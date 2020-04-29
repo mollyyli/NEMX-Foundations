e5bdf4ca829eb15e8acb092c75904825d4d243b4
Molly Li <hsin1128@uw.edu>
2020-04-28 22:46:59 -0700

    push merged stuff

--- code/src/index.js
+++ code/src/index.js
@@ -9,24 +9,26 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { cartReducer } from "./redux/Reducers/cartReducers";
-import * as firebase from 'firebase';
-const firebaseConfig = {
-  apiKey: "AIzaSyCINL3Tc4NsnpM9LGtlRs05C_77ayJttB4",
-  authDomain: "nemx-798a7.firebaseapp.com",
-  databaseURL: "https://nemx-798a7.firebaseio.com",
-  projectId: "nemx-798a7",
-  storageBucket: "nemx-798a7.appspot.com",
-  messagingSenderId: "382671281750",
-  appId: "1:382671281750:web:4f14806e709971dd3365f7",
-  measurementId: "G-DQ9MSL7NED"
-};
-firebase.initializeApp(firebaseConfig);
-var ref = firebase.database().ref();
-console.log(ref)
-const store = createStore(cartReducer);
+// import * as firebase from 'firebase';
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCINL3Tc4NsnpM9LGtlRs05C_77ayJttB4",
+//   authDomain: "nemx-798a7.firebaseapp.com",
+//   databaseURL: "https://nemx-798a7.firebaseio.com",
+//   projectId: "nemx-798a7",
+//   storageBucket: "nemx-798a7.appspot.com",
+//   messagingSenderId: "382671281750",
+//   appId: "1:382671281750:web:4f14806e709971dd3365f7",
+//   measurementId: "G-DQ9MSL7NED"
+// };
+// firebase.initializeApp(firebaseConfig);
+// var ref = firebase.database().ref();
+// console.log(ref)
+// const store = createStore(cartReducer);
 ReactDOM.render(
-  <Provider store={store} >
-    <App db = {ref} />
+  // <Provider store={store} >
+  //   <App db = {ref} />
+  <Provider>
+    <App />
   </Provider>,
   document.getElementById("root")
 );
