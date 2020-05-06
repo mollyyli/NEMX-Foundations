6184081b0baf17fe60ceb9a09b99c92739f1a34c
xifeiwang <xifeiw@uw.edu>
2020-05-05 20:08:56 -0700

    no idea how to fix

--- code/src/pages/Order.js
+++ code/src/pages/Order.js
@@ -82,130 +82,130 @@ const useModalStyles = makeStyles(theme => ({
 
 export default function Order(db) {
 
-  // var all = db.db.ref("fresh")
-  // all.on("value", function(snapshot) {
+  // // var all = db.db.ref("fresh")
+  // // all.on("value", function(snapshot) {
+  // //   console.log(snapshot.val());
+  // //   freshProduceItems = snapshot.val();
+  // // })
+
+  // var meat = db.db.ref("meat")
+  // meat.on("value", function(snapshot) {
   //   console.log(snapshot.val());
-  //   freshProduceItems = snapshot.val();
+  //   meatProteinItems = snapshot.val();
   // })
 
-  var meat = db.db.ref("meat")
-  meat.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    meatProteinItems = snapshot.val();
-  })
-
-  var drink = db.db.ref("drink")
-  drink.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    drinksDairyItems = snapshot.val();
-  })
+  // var drink = db.db.ref("drink")
+  // drink.on("value", function(snapshot) {
+  //   console.log(snapshot.val());
+  //   drinksDairyItems = snapshot.val();
+  // })
 
 
-  var grains = db.db.ref("grains")
-  grains.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    grainsBreadsItems = snapshot.val();
-  })
+  // var grains = db.db.ref("grains")
+  // grains.on("value", function(snapshot) {
+  //   console.log(snapshot.val());
+  //   grainsBreadsItems = snapshot.val();
+  // })
 
 
-  var snacks = db.db.ref("snacks")
-  snacks.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    snacksDessertsItems = snapshot.val();
-  })
+  // var snacks = db.db.ref("snacks")
+  // snacks.on("value", function(snapshot) {
+  //   console.log(snapshot.val());
+  //   snacksDessertsItems = snapshot.val();
+  // })
 
 
-  var soup = db.db.ref("soup")
-  soup.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    soupItems = snapshot.val();
-  })
+  // var soup = db.db.ref("soup")
+  // soup.on("value", function(snapshot) {
+  //   console.log(snapshot.val());
+  //   soupItems = snapshot.val();
+  // })
 
 
-  var spices = db.db.ref("spices")
-  soup.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    spicesCondimentsItems = snapshot.val();
-  })
+  // var spices = db.db.ref("spices")
+  // soup.on("value", function(snapshot) {
+  //   console.log(snapshot.val());
+  //   spicesCondimentsItems = snapshot.val();
+  // })
 
 
-  var all = db.db.ref("fresh")
-  const [fpItems, fpUpdateItems] = useState({});
-  all.once("value").then(function(data){
-    freshProduceItems = data.val();
-    console.log(freshProduceItems)
-    fpUpdateItems(data)
-  })
+  // var all = db.db.ref("fresh")
+
+  // all.once("value").then(function(data){
+  //   freshProduceItems = data.val();
+  //   console.log(freshProduceItems)
+  //   fpUpdateItems(data)
+  // })
  
 
 
-  //db.db.child("/inventory").remove();
-  //db.db.child("/inventory").update(freshProduceItems);
+  // //db.db.child("/inventory").remove();
+  // //db.db.child("/inventory").update(freshProduceItems);
 
-  // Code to extract the inventory from the database to this file
-  useEffect(()=>{
+  // // Code to extract the inventory from the database to this file
+  // useEffect(()=>{
 
   var all = db.db.ref("fresh")
   all.once("value").then(function(data){
-    freshProduceItems = data;
+    freshProduceItems = data.val();
     console.log(freshProduceItems)
-    fpUpdateItems(data)
+    fpUpdateItems(data.val())
   })
 
-  // f.once("value")
-  // .then(function(snapshot) {
-  //   var key = snapshot.key; // null
-  //   var childKey = snapshot.child("users/ada").key; // "ada"
-  // });
+  // // f.once("value")
+  // // .then(function(snapshot) {
+  // //   var key = snapshot.key; // null
+  // //   var childKey = snapshot.child("users/ada").key; // "ada"
+  // // });
 
-  var meat = db.db.ref("meat")
-  meat.on("value", function(snapshot) {
-    meatProteinItems = snapshot.val();
-  })
+  // var meat = db.db.ref("meat")
+  // meat.on("value", function(snapshot) {
+  //   meatProteinItems = snapshot.val();
+  // })
 
-  var drink = db.db.ref("drink")
-  drink.on("value", function(snapshot) {
-    drinksDairyItems = snapshot.val();
-  })
-
-
-  var grains = db.db.ref("grains")
-  grains.on("value", function(snapshot) {
-    grainsBreadsItems = snapshot.val();
-  })
+  // var drink = db.db.ref("drink")
+  // drink.on("value", function(snapshot) {
+  //   drinksDairyItems = snapshot.val();
+  // })
 
 
-  var snacks = db.db.ref("snacks")
-  snacks.on("value", function(snapshot) {
-    snacksDessertsItems = snapshot.val();
-  })
+  // var grains = db.db.ref("grains")
+  // grains.on("value", function(snapshot) {
+  //   grainsBreadsItems = snapshot.val();
+  // })
 
 
-  var soup = db.db.ref("soup")
-  soup.on("value", function(snapshot) {
-    soupItems = snapshot.val();
-  })
+  // var snacks = db.db.ref("snacks")
+  // snacks.on("value", function(snapshot) {
+  //   snacksDessertsItems = snapshot.val();
+  // })
 
 
-  var spices = db.db.ref("spices")
-  spices.on("value", function(snapshot) {
-    spicesCondimentsItems = snapshot.val();
-  })
+  // var soup = db.db.ref("soup")
+  // soup.on("value", function(snapshot) {
+  //   soupItems = snapshot.val();
+  // })
 
 
-    db.db.child("/inventory").on("value", function(snapshot) {
-      for (var i = 0; i < snapshot.numChildren(); i++) {
-        db.db.child("/inventory").child(i).on('value', function(dataSnapshot) {
+  // var spices = db.db.ref("spices")
+  // spices.on("value", function(snapshot) {
+  //   spicesCondimentsItems = snapshot.val();
+  // })
 
-          var newItem = { title: dataSnapshot.val().title, img: dataSnapshot.val().img, 
-            amount: dataSnapshot.val().amount, quantity: dataSnapshot.val().quantity }
-          console.log(newItem);
-          freshProduceItems.push(newItem);
-          console.log("Update: " + freshProduceItems);
-        });
-      }
-    })    
-  },[db]);
+
+  //   db.db.child("/inventory").on("value", function(snapshot) {
+  //     for (var i = 0; i < snapshot.numChildren(); i++) {
+  //       db.db.child("/inventory").child(i).on('value', function(dataSnapshot) {
+
+  //         var newItem = { title: dataSnapshot.val().title, img: dataSnapshot.val().img, 
+  //           amount: dataSnapshot.val().amount, quantity: dataSnapshot.val().quantity }
+  //         console.log(newItem);
+  //         freshProduceItems.push(newItem);
+  //         console.log("Update: " + freshProduceItems);
+  //       });
+  //     }
+  //   })    
+  // },[db]);
   
   //console.log("Fresh produce items: " + freshProduceItems);
   
@@ -246,7 +246,7 @@ export default function Order(db) {
 
 
   // Fresh Produce items
-
+  const [fpItems, fpUpdateItems] = useState(freshProduceItems);
 
   // Meats and Proteins items
   const [mpItems, mpUpdateItems] = useState(meatProteinItems);
